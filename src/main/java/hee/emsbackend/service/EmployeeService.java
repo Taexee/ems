ccaca125facbb9f59a41be814061013a4118ad1e
Taexee <hee66265@naver.com>
2024-01-10 10:56:52 +0900

    feat: 직원 정보 수정 기능 구현

--- src/main/java/hee/emsbackend/service/EmployeeService.java
+++ src/main/java/hee/emsbackend/service/EmployeeService.java
@@ -8,5 +8,5 @@ public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
     EmployeeDto getEmployeeById(Long employeeId);
     List<EmployeeDto> getAllEmployee();
-
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
 }
